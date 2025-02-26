2cd44bd35eea105ac79e27a84e52aac065e0f730
davidwis <davidwis000@gmail.com>
2025-02-26 17:43:53 -0300

    Remove quotes of decodeBencondedInt

--- app/Main.hs
+++ app/Main.hs
@@ -54,7 +54,6 @@ main = do
             -- Uncomment this block to pass stage 1
             let encodedValue = args !! 1
             let decodedValue = decodeBencodedValue $ B.pack encodedValue
-            let jsonValue = encode $ B.unpack decodedValue
-            LB.putStr jsonValue
-            putStr "\n"
+            let jsonValue = B.unpack decodedValue
+            putStrLn jsonValue
         _ -> putStrLn $ "Unknown command: " ++ command
